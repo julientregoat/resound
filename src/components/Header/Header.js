c05a9550117d13973bcef5f9b345c178a1221394
julientregoat <julienctregoat@gmail.com>
2018-03-28 11:42:58 -0400

    styling the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 import Navbar from './Navbar'
-import { Row } from 'antd';
+import { Row, Col } from 'antd';
 
 const Header = ({ user }) => (
   <div className="header">
-    <Row type="flex" justify="space-between" className="header-row">
-      <h1> resound </h1>
-      <Navbar />
-    </Row>
-
-    <Row type="flex" justify="space-between" className="header-row">
-      <span className="header-item">Wallet: {user.wallet}</span>
-      <span>
-        <span className="header-item">Balance: {user.walletBalance} ETH</span>
-        <span className="header-item">|</span>
-        <span className="header-item">Earnings: {user.earningsBalance} ETH</span>
-      </span>
+    <Row type="flex" justify="space-between">
+        <h1> resound </h1>
+        <span>
+          <Navbar />
+          <Col offset={8}>
+            <span className="header-item">Wallet: {user.wallet ? user.wallet.slice(0, 8) : null}...</span>
+            <span className="header-item">|</span>
+            <span className="header-item">Balance: {user.walletBalance} ETH</span>
+            <span className="header-item">|</span>
+            <span className="header-item">Earnings: {user.earningsBalance} ETH</span>
+          </Col>
+        </span>
     </Row>
   </div>
 );
